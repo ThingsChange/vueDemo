6c0b8e5e056ccb84cb05162b4878c518461b5f5a
wanglijun <wanglijun@che001.com>
2018-05-21 16:41:56 +0800

    加入了路由选项

--- src/page/index.vue
+++ src/page/index.vue
@@ -71,27 +71,90 @@
     overflow: hidden;
     height: 100%;
   }
-  .layout .ivu-layout {
+
+  .row-height {
     height: 100%;
   }
-  .layout-logo {
+
+  .layout-breadcrumb {
+    padding: 10px 15px 0;
+  }
+
+  .layout-content {
+    min-height: 600px;
+    margin: 15px;
+    overflow: hidden;
+    background: #fff;
+    border-radius: 4px;
+  }
+
+  .layout-content-main {
+    padding: 10px;
+  }
+
+  .layout-copy {
+    text-align: center;
+    padding: 10px 0 20px;
+    color: #9ea7b4;
+  }
+
+  .layout-menu-left {
+    background: #464c5b;
+  }
+
+  .header-button {
+    padding: 15px 15px;
+    float: right;
+  }
+
+  .layout-header {
+    height: 60px;
+    background: #fff;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+  }
+
+  .layout-logo-left {
     width: 100px;
-    height: 30px;
-    background: #5b6270;
+    height: 100px;
+    /*background: #5b6270;*/
     border-radius: 3px;
-    float: left;
-    position: relative;
-    top: 15px;
-    left: 20px;
+    margin: 15px auto;
+    text-align: center;
   }
-  .layout-nav {
-    width: 420px;
-    margin: 0 auto;
-    margin-right: 20px;
+
+  .layout-logo-left p {
+    color: white;
   }
-  .ivu-menu-item a {
-    display: block;
-    color: #495060;
+
+  .layout-logo-left img {
+    border-radius: 50%;
+  }
+
+  .layout-ceiling-main a {
+    color: #9ba7b5;
+  }
+
+  .layout-hide-text .layout-text {
+    display: none;
+  }
+
+  .ivu-col {
+    transition: width 0.2s ease-in-out;
+  }
+
+  .content-layout {
+    overflow-y: auto;
+  }
+
+  span.layout-hide-text {
+    display: none;
+  }
+  .layout-quit{
+    float:right;
+    line-height:60px;
+    margin-right:30px;
+    color:#9ba7b5;
+    font-weight:bolder;
   }
 </style>
 
