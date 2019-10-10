d5fe865c8c7ed118905a40bc976f31ede21d749e
wanglijun@hualala.com <AMOTEyatou1314>
2019-10-10 17:13:03 +0800

    box-shadow的用法，text-shadow的用法

--- src/page/HelloWorld.vue
+++ src/page/HelloWorld.vue
@@ -10,6 +10,7 @@
           </ul></content>
         <Button class="btn-color">我就是测试</Button>
         <Button type="primary" @click="changeColor">换色</Button>
+        <button>按钮</button>
       </div>
       <SvgIcon name="orderDiscount" width=".16" height=".16" :color="mainColor" class="text" ></SvgIcon>
     </div>
@@ -92,5 +93,28 @@ a {
   .btn-color{
     color: blue;
     background-color: var(--btnColor)
+  }
+  button{
+    outline: none;
+    border: none;
+    color: white;
+    padding: .3rem .1rem;
+    border-radius: .2rem;
+    cursor: pointer;
+    background: linear-gradient(#3de5fb,#26acbd);
+    text-shadow: 0 1px 1px grey;
+    /*位置的三个参数
+          1  X轴方向扩散，负值在左，正值在右
+          2 Y轴方向扩散，正值向下，负值向上
+          3 模糊半径   0时不模糊，就正常的阴影，越大越模糊 类似0实体阴影，数值大的变成了模糊阴影
+          4，颜色可放在这三个数字前面，也可以放在这三个数字后面，这四个参数为一组，可以设置多组。
+          */
+    box-shadow: 0  .2rem 0 #068494,
+                            0 .2rem .2rem grey;
+  }
+  button:active{
+    box-shadow: 0  .1rem 0 #068494,
+    0 .1rem .1rem grey;
+    transform: translate(0,.05rem);
   }
 </style>
