e1d457e6502afa3f8256a34fea4deef8cf3d10b0
wanglijun <wanglijun@hualala.com>
2021-03-23 16:05:50 +0800

    'test'

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import 'iview/dist/styles/iview.css'
 import './assets/css/common.scss'
 import router from './router'
 import axios from './common/axiosConfig'
-import './components/SvgIcon/registerSvg'
+import './components/SvgIcon/registerSvg';;;
 
 // 注册全局组件
 // import './components/SvgIcon/registerSvg'
